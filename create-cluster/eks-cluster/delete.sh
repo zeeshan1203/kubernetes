fe69a1805ca69eb2d46b452786aaa6b79587ce03
S Z Sameeh <zeeshanshaik1203@gmail.com>
2022-02-10 17:13:44 +0530

    added updated and modified

---
+++ create-cluster/eks-cluster/delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+eksctl delete cluster --name sample1 --region us-east-1
---
+++ create-cluster/eks-cluster/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/.kube
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/terraform/install.sh | sudo bash
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/k8-client-stack/install.sh | sudo bash
+curl https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/eksctl/install.sh | sudo bash
+
+eksctl create cluster --name sample1 --region us-east-1 --managed --zones us-east-1a,us-east-1b
